d34faeb04709576e7d2b56253c2bfbff380600f4
sjoodmohammed <sjoodmohammedabdalla@gmail.com>
2023-08-22 18:05:25 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,7 @@ void print_array(int *a, int n)
 {
 	int index_of_array;
 
-	for (index_of_array = 0; index_of_array < 10; index_of_array++)
+	for (index_of_array = 0; index_of_array < n; index_of_array++)
 	{
 		printf("%d", a[index_of_array]);
 		if (index_of_array != (n - 1))
@@ -20,5 +20,5 @@ void print_array(int *a, int n)
 			printf(", ");
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
