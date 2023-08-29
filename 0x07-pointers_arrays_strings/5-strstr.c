4001afdb326758a7bb62cb44e8e5270e502e16b0
sjoodmohammed <sjoodmohammedabdalla@gmail.com>
2023-08-29 02:55:53 -0700

    5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,14 +16,14 @@ if (needle == 0)
 return (haystack);
 while (haystack)
 {
-	index = 0;
+index = 0;
 if (haystack[index] == needle[index])
 {
-	do {
-		if (needle[index + 1] == '\0')
-			return (haystack);
-		index++;
-	} while (haystack[index] == needle[index]);
+do {
+if (needle[index + 1] == '\0')
+return (haystack);
+index++;
+} while (haystack[index] == needle[index]);
 }
 haystack++;
 }
