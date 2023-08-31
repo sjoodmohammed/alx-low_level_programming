9678eaaa3ea68d96626b22301c6794874ef1332f
sjoodmohammed <sjoodmohammedabdalla@gmail.com>
2023-08-30 18:31:59 -0700

    3-factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,12 +8,13 @@
 
 int factorial(int n)
 {
-if (n <= 0)
+if (n == 0)
 {
 return (1);
 }
-else
+else if (n < 0)
 {
-return (n * factorial(n - 1));
+return (-1);
 }
+return (n * factorial(n - 1));
 }
