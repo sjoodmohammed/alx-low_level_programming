ce1f993e29703a66912f9a18886ca31755998d63
sjoodmohammed <sjoodmohammedabdalla@gmail.com>
2023-10-26 20:40:00 -0700

    argv argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - main point.
+* @argc: arg count.
+* @argv: arg vector.
+* Return: 0.
+*/
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+
+}
