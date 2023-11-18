7f4cab6b9544a9c81e7718c88046b86c2c4b5fd4
sjoodmohammed <sjoodmohammedabdalla@gmail.com>
2023-11-18 02:36:52 -0800

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,6 +10,7 @@
 int _strlen(char *s)
 {
 	int i = 0;
+
 	if (!s)
 		return (0);
 	while (*s++)
@@ -26,12 +27,13 @@ int _strlen(char *s)
 
 size_t print_list(const list_t *h)
 {
-	 size_t i = 0;
-	 while (h)
-	 {
-	 	printf("[%d] %s\n", _strlen(h->str), h->str ? h->str :"(nil)");
-	 	h = h->next;
-	 	i++;
-	 }
-	 return (i);
+size_t i = 0;
+
+while (h)
+{
+ 	printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+ 	h = h->next;
+ 	i++;
+}
+return (i);
 }
