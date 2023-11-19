ccb8488af9f7d390cfa7b6204f5b9e3944388daa
sjoodmohammed <sjoodmohammedabdalla@gmail.com>
2023-11-19 06:43:45 -0800

    singly linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+* _constructor - excute before main
+*
+*
+* Return: void
+*/
+
+void _constructor(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
