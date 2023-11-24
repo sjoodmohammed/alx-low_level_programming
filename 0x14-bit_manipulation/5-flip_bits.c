c6252a0e5d853c9f6f6dd3ca6b7e1641493f6232
sjoodmohammed <sjoodmohammedabdalla@gmail.com>
2023-11-24 13:59:52 -0800

    bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,7 +3,7 @@
 /**
 * flip_bits - clear the bit at the endex.
 * @n: the binary number as string
-* @index: the bit to clear
+* @m: the bit to clear
 * Return: the bit state or -1 on error.
 */
 
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xorval = n ^ m;
 	unsigned int count = 0;
-	
+
 	while (xorval)
 	{
 		if (xorval & 1ul)
