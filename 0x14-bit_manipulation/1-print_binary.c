f6d567b4569a414d0f8cffa7de720fd3675a353b
sjoodmohammed <sjoodmohammedabdalla@gmail.com>
2023-11-24 13:50:24 -0800

    bit manipulaton

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,18 +9,18 @@
 
 void print_binary(unsigned long int n)
 {
-	int bit = sizeof(n) * 8, printed = 0;
+int bit = sizeof(n) * 8, printed = 0;
 
-	while (bit)
-	{
-	if (n & 1L << --bit)
-	{
-		_putchar('1');
-		printed++;
-	}
-	else if (printed)
-		 _putchar('0');
-	}
-	if (!printed)
-		_putchar('0');
+while (bit)
+{
+if (n & 1L << --bit)
+{
+	_putchar('1');
+	printed++;
+}
+else if (printed)
+	_putchar('0');
+}
+if (!printed)
+	_putchar('0');
 }
