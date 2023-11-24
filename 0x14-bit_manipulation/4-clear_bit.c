abbb2fee2d8f8c3b1a534a0e561260f4aa7af9a0
sjoodmohammed <sjoodmohammedabdalla@gmail.com>
2023-11-24 13:57:09 -0800

    bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,10 +9,10 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
-			return (-1);
+if (index >= sizeof(n) * 8)
+	return (-1);
 
-	if (*n & 1L << index)
-		*n ^= 1L << index;
-	return (1);
+if (*n & 1L << index)
+	*n ^= 1L << index;
+return (1);
 }
